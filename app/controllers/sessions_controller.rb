a2155e8aef8abda9c9eeac757dcf99d094e4774a
dmaupin <dm@davidmaupin.com>
2015-02-01 10:43:14 -0800

    User and Session Controllers and routes added

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+	def new
+	end
+
+	def create		
+		user = User.where(email: params[:user][:email]).first
+
+		if user && user.authenticate(params[:user][:password])
+			session[:user_id] = user.id 
+			# redirect_to itineraries_path
+		else
+			render :new
+		end
+	end
+
+	def destroy
+		session[:user_id] = nil
+		# redirect_to itineraries_path
+	end
+
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+
+		if @user.save
+			redirect_to beers_path
+		else
+			redirect_to :signup
+		end
+	end
+
+	def user_params
+		params.require(:user).permit(:first_name, :last_name, :email, :age, :gender, :password, :password_confirmation)
+	end
+
+end
