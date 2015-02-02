ef0c6953c6025208dd680f9b282181da72496f99
Greg Rock <grock006@gmail.com>
2015-02-01 20:04:46 -0800

    changed start and end date to text type

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150130222441) do
+ActiveRecord::Schema.define(version: 20150202034053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,8 +36,8 @@ ActiveRecord::Schema.define(version: 20150130222441) do
   create_table "itineraries", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
-    t.datetime "start_date"
-    t.datetime "end_date"
+    t.text     "start_date"
+    t.text     "end_date"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
