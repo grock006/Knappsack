a7ed47b6bf4b7b48f94ce36d80d356a030283eee
Greg Rock <grock006@gmail.com>
2015-02-04 23:19:06 -0800

    added owl carousel

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require example
+//= require main
 //= require angular
 //= require twitter/bootstrap
 //= require bootstrap-datepicker
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
     
     resources :itineraries
+    resources :events
 
 end
