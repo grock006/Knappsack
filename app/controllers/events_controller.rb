73503267d913555ec269edb3abafd15c3346b73d
dmaupin <dm@davidmaupin.com>
2015-02-04 23:53:21 -0800

    Updated Event Controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,9 @@
 class EventsController<ApplicationController
 
 	def index
-		@events = Event.all
+		if current_user
+			@events = Event.all
+		end
 	end
 
 	def new
@@ -9,12 +11,16 @@ class EventsController<ApplicationController
 	end
 
 	def create
-		@event = current_user.event.new(events_parans)
-		if event.save
-			redirect_to events_path
+		@event = Event.new(event_params)
+		if @event.save
+		 	 redirect_to events_path
 		else
-			render :new
+			 redirect_to new_event_path
 		end
+	end
+
+	def event_params
+		params.require(:event).permit(:name, :date, :start_time, :end_time, :category, :description, :rating, :main_url, :location)
 	end
 
 end
