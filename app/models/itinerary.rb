f02b48c17c7208210a0b5d63099061163bd05487
Greg Rock <grock006@gmail.com>
2015-02-05 18:23:30 -0800

    commented out itinerary date validations

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -1,8 +1,8 @@
 class Itinerary < ActiveRecord::Base
 	validates_presence_of :name, :description, :start_date, :end_date
 	validates :name, uniqueness: true
-	validates_date :start_date, :on_or_after => lambda { Date.current}
-	validates_date :end_date, :on_or_after => :start_date
+	# validates_date :start_date, :on_or_after => lambda { Date.current}
+	# validates_date :end_date, :on_or_after => :start_date
 
 	belongs_to :user
 	has_many :events
