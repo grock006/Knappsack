76039d2c80a2e10ad0e2f82df6c40a2cd165f0b0
dmaupin <dm@davidmaupin.com>
2015-02-13 13:07:17 -0800

    Login Path corrected

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,10 +13,6 @@ class ApplicationController < ActionController::Base
 
   end
 
-  def authorize
-    redirect_to root_path unless current_user
-  end
-
   def current_user
       if session[:user_id]
        @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -25,6 +21,7 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user
 
+private
 
   def authorize
     redirect_to login_path unless current_user
