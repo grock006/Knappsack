7deb8237b032e6af4fca484eec418876d0f830c1
dmaupin <dm@davidmaupin.com>
2015-02-13 15:33:36 -0800

    Heroku Prep

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
 
 		if user && user.authenticate(params[:user][:password])
 			session[:user_id] = user.id 
-			redirect_to "/index"
+			redirect_to root_path
 		else
 			# flash[:danger] = "Username or password incorrect."
 			redirect_to root_path
