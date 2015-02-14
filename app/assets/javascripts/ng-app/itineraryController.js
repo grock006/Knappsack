613ad066b696db9f0305205dc737af63381df199
Greg Rock <grock006@gmail.com>
2015-02-13 19:43:37 -0800

    final changes to updated model

--- app/assets/javascripts/ng-app/itineraryController.js
+++ app/assets/javascripts/ng-app/itineraryController.js
@@ -13,28 +13,27 @@ app.controller("itineraryController",function($scope, $http, $resource){
     // // Click on events in calendar and events appear below Calendar
     $scope.alertEventOnClick = function(data) {
             $scope.show = data
-            console.log(data);
             $scope.showevent = true;
         };
 
-  //Configuration for the Angular UI Calendar     
-    $scope.uiConfig = {
-      calendar:{
-        height: 350,
-        editable: false,
-        timezone: false,
-        header:{
-          left: 'month agendaWeek agendaDay',
-          center: 'title',
-          right: 'today prev,next'
-        },
-        eventClick: $scope.alertEventOnClick,   
-        eventMouseover: $scope.alertEventOnMouseover,     
-        eventDrop: $scope.alertOnDrop,
-        eventResize: $scope.alertOnResize,
-        dayClick: $scope.alertDayOnClick
-      }
-    }
+      //Configuration for the Angular UI Calendar     
+        $scope.uiConfig = {
+          calendar:{
+            height: 350,
+            editable: false,
+            timezone: false,
+            header:{
+              left: 'month agendaWeek agendaDay',
+              center: 'title',
+              right: 'today prev,next'
+            },
+            eventClick: $scope.alertEventOnClick,   
+            eventMouseover: $scope.alertEventOnMouseover,     
+            eventDrop: $scope.alertOnDrop,
+            eventResize: $scope.alertOnResize,
+            dayClick: $scope.alertDayOnClick
+          }
+        }
      
 });
 
