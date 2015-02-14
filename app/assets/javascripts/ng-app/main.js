ec98c83747627799f142e6aaa83c963dc943cdf1
Greg Rock <grock006@gmail.com>
2015-02-13 17:30:24 -0800

    lil

--- app/assets/javascripts/ng-app/main.js
+++ app/assets/javascripts/ng-app/main.js
@@ -7,12 +7,4 @@ angular
     .config(function($httpProvider, $stateProvider, $urlRouterProvider) {
     $httpProvider.defaults.headers.common['X-CSRF-Token'] = 
       $('meta[name=csrf-token]').attr('content');
-  
-	// $stateProvider
- //       .state("newfood", {
- //        url: "/food/new",
- //        templateUrl: "food/new.html",
- //        controller: newFoodController
- //      })
- //      ;
   })
